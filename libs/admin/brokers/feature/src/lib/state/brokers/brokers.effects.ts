d632cc781bc87d48b37cfb0584af69e95c291702
Mark Goho <markgoho@gmail.com>
2020-02-06 10:40:48 -0500

    update loadBrokers effect to use data persistence module

--- libs/admin/brokers/feature/src/lib/state/brokers/brokers.effects.ts
+++ libs/admin/brokers/feature/src/lib/state/brokers/brokers.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { fetch } from '@nrwl/angular';
+import { DataPersistence } from '@nrwl/angular';
 import { map } from 'rxjs/operators';
 
 import { BrokersApiService } from '@hbx/admin/brokers/data-access';
@@ -11,27 +11,31 @@ import * as BrokersActions from './brokers.actions';
 @Injectable()
 export class BrokersEffects {
   loadBrokers$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(BrokersActions.loadBrokers),
-      fetch({
-        run: action => {
-          return this.brokerApiService
-            .getAllBrokers()
-            .pipe(
-              map(brokers => BrokersActions.loadBrokersSuccess({ brokers: [] }))
-            );
-        },
+    this.dataPersistence.fetch(BrokersActions.loadBrokers, {
+      run: (
+        action: ReturnType<typeof BrokersActions.loadBrokers>,
+        state: fromBrokers.BrokersPartialState
+      ) => {
+        return this.brokersApiService
+          .getAllBrokers()
+          .pipe(
+            map(brokers => BrokersActions.loadBrokersSuccess({ brokers: [] }))
+          );
+      },
 
-        onError: (action, error) => {
-          console.error('Error', error);
-          return BrokersActions.loadBrokersFailure({ error });
-        },
-      })
-    )
+      onError: (
+        action: ReturnType<typeof BrokersActions.loadBrokers>,
+        error
+      ) => {
+        console.error('Error', error);
+        return BrokersActions.loadBrokersFailure({ error });
+      },
+    })
   );
 
   constructor(
     private actions$: Actions,
-    private brokerApiService: BrokersApiService
+    private dataPersistence: DataPersistence<fromBrokers.BrokersPartialState>,
+    private brokersApiService: BrokersApiService
   ) {}
 }
