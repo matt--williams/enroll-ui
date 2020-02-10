d0e74eda576373dba4bc69ed0837abcf49d2429b
Mark Goho <markgoho@gmail.com>
2020-02-10 10:04:27 -0500

    add additional compiler options

--- apps/admin-e2e/src/integration/app.spec.ts
+++ apps/admin-e2e/src/integration/app.spec.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
-import { mockBasePerson, mockGeneralAgency } from '@hbx/utils/testing';
-
 describe('admin', () => {
   beforeEach(() => {
     cy.server();
-    cy.route('**/people', [mockBasePerson()]).as('people');
-    cy.route('**/organizations', [mockGeneralAgency('Broker')]).as(
-      'organizations'
-    );
+    // cy.route('**/people', [mockBasePerson()]).as('people');
+    // cy.route('**/organizations', [mockGeneralAgency('Broker')]).as(
+    //   'organizations'
+    // );
     cy.visit('/');
   });
 
