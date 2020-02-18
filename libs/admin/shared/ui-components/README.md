782588047cb19a5ed4c99008c58d3b7cabb428c6
Mark Goho <markgoho@gmail.com>
2020-02-18 09:51:49 -0500

    add ui components lib for admin

---
+++ libs/admin/shared/ui-components/README.md
@@ -0,0 +1,7 @@
+# admin-shared-ui-components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test admin-shared-ui-components` to execute the unit tests.
---
+++ libs/admin/shared/ui-components/src/lib/agency-staff/agency-staff.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgencyStaffComponent } from './agency-staff.component';
+
+@NgModule({
+  declarations: [AgencyStaffComponent],
+  imports: [CommonModule],
+  exports: [AgencyStaffComponent],
+})
+export class AgencyStaffModule {}
